faf81eae90af3295875b61fc7e45a2c5e07da4d9
Larissa <Larissa@DESKTOP-N0IML8E>
2023-05-01 20:01:40 +0200

    Add project files.

---
+++ Einheit11/Adressverwaltung.cs
@@ -0,0 +1,16 @@
+﻿namespace Einheit11
+{
+  public partial class Adressverwaltung : Form
+  {
+    public Adressverwaltung()
+    {
+      InitializeComponent();
+    }
+
+    private void CmdCreatePerson_Click(object sender, EventArgs e)
+    {
+
+    }
+
+  }
+}
---
+++ ZbW.PF2.FS23.B/Form1.cs
@@ -0,0 +1,10 @@
+namespace ZbW.PF2.FS23.B
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+  }
+}
