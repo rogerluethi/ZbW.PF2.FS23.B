023d65e419c630d13b82f690ca157a72bbd17a43
Larissa <larissa.fitze@hotmail.com>
2023-05-16 20:02:16 +0200

    Beispiele

---
+++ Einheit11/Color/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einheit11.Color
+{
+    internal class Color
+    {
+    }
+}
---
+++ Einheit11/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einheit11
+{
+    public class Person
+    {
+        private string firstName = "Standard Name";
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value != null ? value : "not define"; }
+        }
+
+        public string LastName { get; set; }
+
+    }
+}
